8b83d11117c5374f758e69e754c073745ab8d345
Diego Costa <48633894+diegocosta-dev@users.noreply.github.com>
2025-08-27 21:46:22 -0300

    change astro condig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,13 @@
-// @ts-check
+// astro.config.ts
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [alpinejs({
-    entrypoint: "/src/alpine-entry",
-  }), react()],
-
+  output: "server",
+  vite: { plugins: [tailwindcss()] },
+  integrations: [alpinejs({ entrypoint: "/src/alpine-entry" }), react()],
   adapter: cloudflare(),
 });
