8c34587fc87acc5771a1bbf279479d3794b53abe
Diego Costa <48633894+diegocosta-dev@users.noreply.github.com>
2025-08-28 12:12:35 -0300

    add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import alpinejs from "@astrojs/alpinejs";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   output: "server",
+  site: "https://talitacamilo.pages.dev",
   vite: { plugins: [tailwindcss()] },
-  integrations: [alpinejs({ entrypoint: "/src/alpine-entry" }), react()],
+  integrations: [
+    alpinejs({ entrypoint: "/src/alpine-entry" }),
+    react(),
+    sitemap(),
+  ],
   adapter: cloudflare(),
 });
